d4e8cbe97b48861654201cb4c5a93f5513d51a1f
Sleep-Machine <ohnoitsatornahdo@gmail.com>
2021-05-02 16:25:45 -0700

    updated movement and basic collison working

--- scenes/gameplay_ph/player/movement.gd
+++ scenes/gameplay_ph/player/movement.gd
@@ -1,12 +1,16 @@
 extends KinematicBody2D
 
-func _process(delta):
-	var speed = 200;
-	if Input.is_action_pressed("ui_left"):
-		position.x -= speed * delta
-	if Input.is_action_pressed("ui_right"):
-		position.x += speed * delta
-	if Input.is_action_pressed("ui_up"):
-		position.y -= speed * delta
-	if Input.is_action_pressed("ui_down"):
-		position.y += speed * delta
+# Create a velocity zero vector <0,0>
+var vel = Vector2.ZERO
+var speed = 200
+
+# Gets input
+func get_input():
+	# Returns the input values
+	vel.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * speed
+	vel.y = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")) * speed
+	
+# Handles physics processes including collision
+func _physics_process(_delta):
+	get_input()
+	vel = move_and_slide(vel)
