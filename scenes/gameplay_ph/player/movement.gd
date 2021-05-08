3b3e9f14a8400b29152cea8b881f09d8c96a7606
Sleep-Machine <ohnoitsatornahdo@gmail.com>
2021-05-08 15:44:05 -0700

    Got sprite animations and movement working

--- scenes/gameplay_ph/player/movement.gd
+++ scenes/gameplay_ph/player/movement.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 # Create a velocity zero vector <0,0>
 var vel = Vector2.ZERO
 var speed = 200
+var animation = "Idle"
 
 # Handles physics processes including collision
 func _physics_process(delta):
@@ -13,5 +14,22 @@ func _physics_process(delta):
 	input.y = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up"))
 	
 	vel = input.normalized() * speed * 60 * delta
-	
 	vel = move_and_slide(vel)
+	
+	handle_sprite(input, vel)
+	
+func handle_sprite(input, vel):
+	# Set animation to variable animation
+	$PLAYER_Sprite.play(animation)
+	
+	# If the player is not moving, set the animation to "Idle", otherwise set it to run
+	if vel == Vector2.ZERO:
+		animation = "Idle"
+	else:
+		animation = "Run"
+	
+	# Flip sprite depending on what direction the player is facing
+	if input.x == -1:
+		$PLAYER_Sprite.flip_h = true
+	if input.x == 1:
+		$PLAYER_Sprite.flip_h = false
