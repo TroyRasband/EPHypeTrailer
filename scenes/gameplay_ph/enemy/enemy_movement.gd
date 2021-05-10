0d492aab8d8933976746dc30e7cfbe6bce9309b0
Sleep-Machine <ohnoitsatornahdo@gmail.com>
2021-05-10 14:36:07 -0700

    Added enemy sprites + experimenting with enum + knockback babyyy

--- scenes/gameplay_ph/enemy/enemy_movement.gd
+++ scenes/gameplay_ph/enemy/enemy_movement.gd
@@ -5,16 +5,80 @@ var vel = Vector2.ZERO
 var speed = 120
 var is_hurt = false
 var health = 2
+var animation = "Move"
+
+var dir
+
+var knockback = Vector2.ZERO
+
+# States for enemy
+var state
+var type
 
 # Obtain the player
 onready var player = get_parent().get_node("PLAYER")
+onready var animation_player = $AnimationPlayer
 
-func _physics_process(_delta):
+# Expermenting with state machines for enemy
+enum state_machine_enemy {
+	MOVE,
+	HIT,
+	ATTACK
+}
+
+enum direction {
+	LEFT,
+	RIGHT
+}
+
+enum level_type {
+	ONE,
+	TWO,
+	THREE,
+	FOUR
+}
+
+func _ready():
+	state = state_machine_enemy.MOVE
+	type = level_type.ONE
+
+func _physics_process(delta):
+	handle_sprite()
+	print(dir)
 	vel = (player.position - position).normalized() * speed
-	vel = move_and_slide(vel.floor())
+	if state == state_machine_enemy.MOVE:
+		vel = move_and_slide(vel)
+		if vel.x > 1:
+			dir = direction.RIGHT
+		if vel.x < -1:
+			dir = direction.LEFT
+	if vel.length() <= 30:
+		change_state(state_machine_enemy.ATTACK)
+	if state == state_machine_enemy.HIT:
+		knockback = knockback.move_toward(Vector2.ZERO, 1000 * delta)
+		knockback = move_and_slide(knockback)
 	
 func get_health():
 	return health
 	
 func set_health(health):
 	self.health = health
+
+func change_state(_state):
+	state = _state
+
+func change_state_default():
+	state = state_machine_enemy.MOVE
+
+func handle_sprite():
+	if (state == state_machine_enemy.MOVE):
+		animation = "Move"
+	if (state == state_machine_enemy.HIT):
+		animation = "Hit"
+	if (state == state_machine_enemy.ATTACK):
+		animation = "Attack"
+	if (dir == 1):
+		$ENEMY_Sprite.set_flip_h(false)
+	if (dir == 0):
+		$ENEMY_Sprite.set_flip_h(true)
+	animation_player.play(animation)
--- scenes/gameplay_ph/hurtbox.gd
+++ scenes/gameplay_ph/hurtbox.gd
@@ -5,7 +5,9 @@ var is_hurt = false
 onready var enemy = get_parent()
 
 func get_hurt():
-	if enemy.get_health() > 0:
-		enemy.set_health(enemy.get_health() - 1)
-	else:
-		enemy.queue_free()
+	if (enemy.dir == enemy.direction.RIGHT):
+		enemy.knockback = Vector2.LEFT * 1000
+	if (enemy.dir == enemy.direction.LEFT):
+		enemy.knockback = Vector2.RIGHT * 1000
+	enemy.animation_player.stop()
+	enemy.change_state(enemy.state_machine_enemy.HIT)
