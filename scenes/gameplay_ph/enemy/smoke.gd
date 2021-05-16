d79db7a17dcb7693a6b774d1b03dd5f16065e85e
Sleep-Machine <ohnoitsatornahdo@gmail.com>
2021-05-16 01:47:19 -0700

    Added logo

--- scenes/gameplay_ph/enemy/smoke.gd
+++ scenes/gameplay_ph/enemy/smoke.gd
@@ -5,5 +5,5 @@ onready var player = get_parent().get_node("Smoke_Player")
 func _ready():
 	player.play("Smoke")
 
-func _on_Smoke_Player_animation_finished():
+func _on_Smoke_Player_animation_finished(anim_name):
 	player.stop()
--- scenes/level_select/select_level.gd
+++ scenes/level_select/select_level.gd
@@ -1,6 +1,26 @@
 extends Control
 
-# Press space to change scene to selected level
-func _input(_event):
-	if Input.is_key_pressed(KEY_SPACE):
-		get_tree().change_scene("res://scenes/gameplay_ph/demo.tscn")
+var level = 0
+onready var button_r = $Button_2
+onready var button_l = $Button_1
+onready var select = $Select
+
+# Update text by getting the node and actively setting the text to the counter value
+func _process(_delta):
+	# Quirky modulus math that returns the index of the level
+	level = abs(fmod(button_r.counter + button_l.counter, 4))
+	if level == 0:
+		ed(true, false)
+	elif level == 3:
+		ed(false, true)
+	else:
+		ed(false, false)
+
+func _on_Select_pressed():
+	get_tree().change_scene("res://scenes/gameplay_ph/demo.tscn")
+
+# Function used to disable/enable the buttons
+func ed(b1, b2):
+	button_l.disabled = b1
+	button_r.disabled = b2
+
