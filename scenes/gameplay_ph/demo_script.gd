c575721e179b9a4b97d5ebc6edd93835d8a7cd72
Sleep-Machine <ohnoitsatornahdo@gmail.com>
2021-06-21 23:23:57 -0700

    Added game over screen

---
+++ scenes/gameplay_ph/demo_script.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+onready var player = get_node("YSort/PLAYER")
+
+func _process(delta):
+	if (player.health <= 0):
+		$CanvasLayer/Flash/AnimationPlayer.play("flash")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	get_tree().change_scene("res://scenes/game_over/game_over.tscn")
--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,6 +1,10 @@
 extends Control
 
+func _ready():
+	$ColorRect/AnimationPlayer.play("fade_out")
+
 # Press space to change scene to level select
 func _input(_event):
 	if Input.is_key_pressed(KEY_SPACE):
 		get_tree().change_scene("res://scenes/level_select/level_select.tscn")
+	
