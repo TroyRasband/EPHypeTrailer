040bfb6ba8fbf11e1257e4344d47e8b41bb49773
Frgatso <57468996+Frgatso@users.noreply.github.com>
2021-07-20 19:39:56 -0700

    Delete Smoke.gd
    Deleted Smoke.gd to prevent exporting issues

--- scenes/gameplay_ph/enemy/Smoke.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite
-
-onready var player = get_parent().get_node("Smoke_Player")
-
-func _ready():
-	player.play("Smoke")
-
-func _on_Smoke_Player_animation_finished(anim_name):
-	player.stop()
