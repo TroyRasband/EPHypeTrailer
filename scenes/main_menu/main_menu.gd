6098fc96d2d94d1de88b33250f57f9a5bacc0920
Frgatso <57468996+Frgatso@users.noreply.github.com>
2021-11-04 23:19:06 -0700

    Added Controller Support

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -7,3 +7,7 @@ func _ready():
 func _on_TouchScreenButton_pressed():
 	get_tree().change_scene("res://scenes/level_select/level_select.tscn")
 	pass # Replace with function body.
+func _process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().change_scene("res://scenes/level_select/level_select.tscn")
+		pass
